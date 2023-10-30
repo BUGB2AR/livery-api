a39da4e8344e039c738217b7b370acc5b96d183f
WebSh4dow <phantomrising@protonmail.com>
2023-10-29 22:24:02 -0300

    first commit

---
+++ src/main/java/com/jarmisondev/liveryapi/LiveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.jarmisondev.liveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jarmisondev/liveryapi/LiveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarmisondev.liveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
