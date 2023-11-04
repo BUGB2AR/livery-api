92307e91bc05973586b0da4b728796ad17ab3045
WebSh4dow <phantomrising@protonmail.com>
2023-11-04 11:22:47 -0300

    colocando o metodo de adicionar como salvar e atualizar

--- src/main/java/com/jarmisondev/liveryapi/jpa/AlteracaoCozinhaMain.java
+++ src/main/java/com/jarmisondev/liveryapi/jpa/AlteracaoCozinhaMain.java
@@ -5,7 +5,7 @@ import com.jarmisondev.liveryapi.domain.model.Cozinha;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-public class InclusaoCozinhaMain {
+public class AlteracaoCozinhaMain {
     public static void main(String[] args) {
         org.springframework.context.ApplicationContext applicationContext =  new SpringApplicationBuilder(LiveryApiApplication.class)
                 .web(WebApplicationType.NONE)
@@ -13,16 +13,10 @@ public class InclusaoCozinhaMain {
 
         CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 
-        Cozinha cozinha1 = new Cozinha();
-        cozinha1.setNome("Brasileira");
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome("Brasileira");
 
-        Cozinha cozinha2 = new Cozinha();
-        cozinha2.setNome("Japonesa");
-
-        cozinha1 = cadastroCozinha.salvar(cozinha1);
-        cozinha2 = cadastroCozinha.salvar(cozinha2);
-
-        System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
-        System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
+        cadastroCozinha.salvar(cozinha);
     }
 }
