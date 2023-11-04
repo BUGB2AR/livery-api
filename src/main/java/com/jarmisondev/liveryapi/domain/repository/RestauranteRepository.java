608c2eec10467c376bd5624424526d150da436db
WebSh4dow <phantomrising@protonmail.com>
2023-11-04 13:48:49 -0300

    adicionado restaurante implements

---
+++ src/main/java/com/jarmisondev/liveryapi/domain/repository/RestauranteRepository.java
@@ -0,0 +1,12 @@
+package com.jarmisondev.liveryapi.domain.repository;
+
+import com.jarmisondev.liveryapi.domain.model.Restaurante;
+
+import java.util.List;
+
+public interface RestauranteRepository {
+    List<Restaurante> todos();
+    Restaurante adicionar(Restaurante restaurante);
+    void remover(Restaurante restaurante);
+    Restaurante porId(Long id);
+}
