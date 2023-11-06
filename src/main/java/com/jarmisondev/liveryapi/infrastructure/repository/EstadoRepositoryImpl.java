e3998e9201170387d75cf492c59c55008648158e
FireXtz <phantomrising@protonmail.com>
2023-11-06 17:39:15 -0300

    colocando resource de estados identaçãode codigo

--- src/main/java/com/jarmisondev/liveryapi/infrastructure/repository/EstadoRepositoryImpl.java
+++ src/main/java/com/jarmisondev/liveryapi/infrastructure/repository/EstadoRepositoryImpl.java
@@ -1,16 +1,18 @@
 package com.jarmisondev.liveryapi.infrastructure.repository;
 
+import com.jarmisondev.liveryapi.domain.model.Cidade;
 import com.jarmisondev.liveryapi.domain.model.Estado;
+import com.jarmisondev.liveryapi.domain.repository.CidadeRepository;
 import com.jarmisondev.liveryapi.domain.repository.EstadoRepository;
-import javax.persistence.EntityManager;
-import java.util.List;
 import org.springframework.stereotype.Component;
-import javax.persistence.PersistenceContext;
+
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Component
 public class EstadoRepositoryImpl implements EstadoRepository {
-    @PersistenceContext
+
     private EntityManager entityManager;
     @Override
     public List<Estado> todos() {
@@ -19,22 +21,22 @@ public class EstadoRepositoryImpl implements EstadoRepository {
 
     @Override
     @Transactional
-    public void remover(Estado estado) {
-        if (estado.getId() != null){
-            entityManager.remove(estado);
-        }else{
-            System.out.println("Estado remido ou não existe cadastrado!!!");
-        }
+    public Estado adicionar(Estado estado) {
+        return entityManager.merge(estado);
     }
 
     @Override
     public Estado porId(Long id) {
-       return entityManager.find(Estado.class,id);
+        return entityManager.find(Estado.class,id);
     }
 
     @Override
     @Transactional
-    public Estado adicionar(Estado estado) {
-        return entityManager.merge(estado);
+    public void remover(Estado estado) {
+        if (estado.getId() != null){
+            entityManager.remove(estado);
+        }else{
+            System.out.println("Estado não existe ou foi removida!!!!");
+        }
     }
 }
