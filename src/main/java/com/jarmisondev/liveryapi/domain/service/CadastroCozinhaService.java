6bfcf8cb21f97b3d93aaab28f636e92f6bfc8c60
FireXtz <phantomrising@protonmail.com>
2023-11-11 11:37:57 -0300

    adicionei o remover cozinhas e tratamento de novas exceptions

--- src/main/java/com/jarmisondev/liveryapi/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/jarmisondev/liveryapi/domain/service/CadastroCozinhaService.java
@@ -1,8 +1,12 @@
 package com.jarmisondev.liveryapi.domain.service;
 
+import com.jarmisondev.liveryapi.domain.exception.EntidadeEmUsoException;
+import com.jarmisondev.liveryapi.domain.exception.EntidadeNaoEncontradaException;
 import com.jarmisondev.liveryapi.domain.model.Cozinha;
 import com.jarmisondev.liveryapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,5 +18,16 @@ public class CadastroCozinhaService {
 
     public Cozinha salvar(Cozinha cozinha){
         return cozinhaRepository.adicionar(cozinha);
+    }
+
+    public void excluir(Cozinha cozinha){
+        try {
+            cozinhaRepository.remover(cozinha);
+
+        }catch(EmptyResultDataAccessException e){
+            throw new EntidadeNaoEncontradaException("Não existe um cadastro de cozinha atual.");
+        }catch (DataIntegrityViolationException e){
+            throw new EntidadeEmUsoException("Cozinha atual não pode ser removida, pois está em uso");
+        }
     }
 }
