3dedb3b8c67a5add9bb390b60bb7a39b8af61f96
FireXtz <phantomrising@protonmail.com>
2024-01-06 15:30:50 -0300

    Usando as keywords para definir critérios de query methods

--- src/main/java/com/jarmisondev/liveryapi/api/controller/ControllerTeste.java
+++ src/main/java/com/jarmisondev/liveryapi/api/controller/ControllerTeste.java
@@ -1,22 +1,45 @@
 package com.jarmisondev.liveryapi.api.controller;
 
 import com.jarmisondev.liveryapi.domain.model.Cozinha;
+import com.jarmisondev.liveryapi.domain.model.Restaurante;
 import com.jarmisondev.liveryapi.domain.repository.CozinhaRepository;
+import com.jarmisondev.liveryapi.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/teste")
+@RestController
 public class ControllerTeste {
 
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping("/cozinhas/por-nome")
-    public List<Cozinha> listarCozinhasPorNome(@RequestParam String nome){
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @GetMapping("/cozinhas/unica-por-nome")
+    public Optional<Cozinha> listarCozinhasPorNome(@RequestParam String nome){
         return cozinhaRepository.findByNome(nome);
     }
 
+    @GetMapping("/cozinhas/por-nome")
+    public List<Cozinha> cozinhasPorNome(String nome) {
+        return cozinhaRepository.findTodasByNomeContaining(nome);
+    }
+
+    @GetMapping("/restaurantes/por-taxa-frete")
+    public List<Restaurante> restaurantesPorTaxaFrete(BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
+        return restauranteRepository.findByTaxaFreteBetween(taxaFreteInicial,taxaFreteFinal);
+    }
+
+    @GetMapping("/restaurantes/por-nome")
+    public List<Restaurante> restaurantesPorNome(String nome, Long cozinha){
+        return restauranteRepository.findByNomeContainingAndCozinhaId(nome,cozinha);
+    }
 }
--- src/main/java/com/jarmisondev/liveryapi/domain/repository/CozinhaRepository.java
+++ src/main/java/com/jarmisondev/liveryapi/domain/repository/CozinhaRepository.java
@@ -4,8 +4,11 @@ import com.jarmisondev.liveryapi.domain.model.Cozinha;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CozinhaRepository extends JpaRepository<Cozinha,Long> {
-    List <Cozinha> findByNome(String nome);
+    Optional<Cozinha> findByNome(String nome);
+
+    List<Cozinha> findTodasByNomeContaining(String nome);
 
 }
