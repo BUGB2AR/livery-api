b4dd51403e92d079b27c6b7a662ec98227d2bf34
WebSh4dow <phantomrising@protonmail.com>
2024-11-04 20:16:49 -0300

    Estendendo o JpaRepository para customizar o repositório

--- src/main/java/com/jarmisondev/liveryapi/LiveryApiApplication.java
+++ src/main/java/com/jarmisondev/liveryapi/LiveryApiApplication.java
@@ -2,8 +2,12 @@ package com.jarmisondev.liveryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.jarmisondev.liveryapi.infrastructure.repository.CustomJpaRepositoryImpl;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class LiveryApiApplication {
 
 	public static void main(String[] args) {
